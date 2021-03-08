3af5c064f5cae78161dc97c9acb914ea097fd633
yasmeenkhalili <jasmina201132@yahoo.com>
2021-03-08 07:20:25 +0200

    link sales.html with app.js

--- js/app.js
+++ js/app.js
@@ -1,9 +1,9 @@
 'use strict';
 let hours=['6am','7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm'];
+let main=document.getElementById('demo');
 function randomGenerator(minCustConsumption,maxCustConsumption,avgCookiePerSale){
   return Math.floor(Math.random() * ((maxCustConsumption-minCustConsumption+1)+minCustConsumption) * avgCookiePerSale);
 }
-let main=document.getElementById('demo');
 function seattle(){
   let Seattle={
     minCustConsumption:23,
