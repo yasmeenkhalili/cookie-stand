24047402b6f43ab91d516784825ab4666b9ebea3
yasmeenkhalili <jasmina201132@yahoo.com>
2021-03-09 16:31:39 +0200

    calculate the total and the total of total

--- js/app.js
+++ js/app.js
@@ -2,9 +2,12 @@
 function randomGenerator(minCust, maxCust, avgPerSale) {
   return Math.floor(Math.random() * ((maxCust - minCust + 1) + minCust) * avgPerSale);
 }
+
+
 let hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
 let main = document.getElementById('demo');
 let table = document.createElement('table');
+
 function hour() {
   main.appendChild(table);
   let cookiesRow = document.createElement('tr');
@@ -19,7 +22,9 @@ function hour() {
   heading = document.createElement('th');
   cookiesRow.appendChild(heading);
   heading.innerText = 'Daily Total';
-} hour();
+}hour();
+
+
 function ParametersOfLocations(minCust, maxCust, avgPerSale, storeName) {
   this.minCust = minCust;
   this.maxCust = maxCust;
@@ -27,29 +32,21 @@ function ParametersOfLocations(minCust, maxCust, avgPerSale, storeName) {
   this.storeName = storeName;
   this.dailyTotal = 0;
   this.totals=0;
-  this.hourtotal=function(){
-    let cookiesRow = document.createElement('tr');
-    let data2 = document.createElement('td');
-    table.appendChild(cookiesRow);
-    cookiesRow.appendChild(data2);
-    data2.innerText = ' Totals ';
-  };
+  this.summationAvgCookiesPerHour=[];
   this.render = function () {
     let cookiesRow = document.createElement('tr');
-    let heading = document.createElement('td');
-    let summation=[];
+    let heading = document.createElement('th');
     heading.innerText = this.storeName;
     table.appendChild(cookiesRow);
     cookiesRow.appendChild(heading);
     for (let i = 0; i < hours.length; i++) {
       let data = document.createElement('td');
       cookiesRow.appendChild(data);
-      let avgCookies = randomGenerator(this.minCust, this.maxCust, this.avgPerSale);
-      summation[i]=avgCookies;
-      this.dailyTotal = this.dailyTotal + avgCookies;
-      data.innerText = avgCookies;
+      let avgCookiesPerHour = randomGenerator(this.minCust, this.maxCust, this.avgPerSale);
+      this.summationAvgCookiesPerHour[i]=avgCookiesPerHour;
+      this.dailyTotal = this.dailyTotal + avgCookiesPerHour;
+      data.innerText = avgCookiesPerHour;
     }
-    console.log(summation);
     let data = document.createElement('td');
     cookiesRow.appendChild(data);
     data.innerText = this.dailyTotal;
@@ -57,6 +54,7 @@ function ParametersOfLocations(minCust, maxCust, avgPerSale, storeName) {
 
   };
 }
+
 
 let Seattle = new ParametersOfLocations(23, 65, 6.3, 'Seattle');
 Seattle.render();
@@ -68,4 +66,26 @@ let Paris = new ParametersOfLocations(20, 38, 2.3, 'Paris');
 Paris.render();
 let Lima = new ParametersOfLocations(2, 16, 4.6, 'Lima');
 Lima.render();
-Lima.hourtotal();
+
+function total(){
+  main.appendChild(table);
+  let cookiesRow = document.createElement('tr');
+  table.appendChild(cookiesRow);
+  let heading = document.createElement('th');
+  cookiesRow.appendChild(heading);
+  heading.innerText = 'Total';
+  let sum=[];
+  for(let i=0;i<hours.length;i++){
+    let data = document.createElement('td');
+    cookiesRow.appendChild(data);
+    sum[i]=Seattle.summationAvgCookiesPerHour[i]+Tokyo.summationAvgCookiesPerHour[i]+Dubai.summationAvgCookiesPerHour[i]+Paris.summationAvgCookiesPerHour[i]+Lima.summationAvgCookiesPerHour[i];
+    data.innerText =sum[i];
+    table.appendChild(cookiesRow);
+  }
+  let data = document.createElement('td');
+  cookiesRow.appendChild(data);
+  let sumOFSumTotal=Seattle.dailyTotal+Tokyo.dailyTotal+Dubai.dailyTotal+Paris.dailyTotal+Lima.dailyTotal;
+  data.innerText=sumOFSumTotal;
+  table.appendChild(cookiesRow);
+
+}total();
